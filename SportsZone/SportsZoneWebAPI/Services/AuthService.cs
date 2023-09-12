d2f51e82ce37ba8cc2d7a9ae8fe7e3fa878924d5
Sathish KS <Sathish.KS@eschergroup.com>
2023-09-12 13:48:05 +0530

    added auth service

---
+++ SportsZone/SportsZoneWebAPI/Services/AuthService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsZoneWebAPI.Services.Interfaces;
+using SportsZoneWebAPI.Repositories.Interfaces;
+using SportsZoneWebAPI.DTOs;
+
+namespace SportsZoneWebAPI.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IAuthRepository _authRepository;
+
+        public AuthService(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+
+        public async Task<bool> IsAuthenticatedUser(string email, string password)
+        {
+            try
+            {
+                return await _authRepository.IsValidEmail(email) && await _authRepository.IsValidPassword(password);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<string> GenerateJwtToken(LoginDTO loginDTO)
+        {
+            try
+            {
+                return await _authRepository.GenerateJwtToken(loginDTO);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
