c7796bfbc67a803d7e5598584307328b40420a90
Sathish KS <Sathish.KS@eschergroup.com>
2023-10-05 10:19:26 +0530

    Presenting to Shri

---
+++ TimeSavers/DemoRefactor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSavers
+{
+    class DemoRefactor
+    {
+        public void ProcessData()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int sum = 0;
+            int product = 1;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            foreach (int num in numbers)
+            {
+                // Calculate the sum
+                sum += num;
+
+                // Calculate the product
+                product *= num;
+
+                // Find the maximum
+                if (num > max)
+                {
+                    max = num;
+                }
+
+                // Find the minimum
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+
+            // Print the results
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Product: " + product);
+            Console.WriteLine("Maximum: " + max);
+            Console.WriteLine("Minimum: " + min);
+        }
+
+    }
+
+}
---
+++ TimeSavers/RenamingAnIdentifier.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSavers
+{
+    class RenamingAnIdentifier
+    {
+    }
+
+    public class Calculator
+    {
+        private readonly double num1;
+        private readonly double num2;
+
+        public Calculator(double num1, double num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        // Add two numbers
+        public double Add()
+        {
+            return num1 + num2;
+        }
+
+        // Subtract two numbers
+        public double Subtract()
+        {
+            return num1 - num2;
+        }
+
+        // Multiply two numbers
+        public double Multiply()
+        {
+            return num1 * num2;
+        }
+
+        // Divide two numbers
+        public double Divide()
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return num1 / num2;
+        }
+    }
+
+}
